cb1f1c23d707f68f881c6b68fd94e811d9bed207
David <davidptsmith@gmail.com>
2022-05-05 12:27:48 +0800

    About page options

--- components/about_page/AboutSection.tsx
+++ components/about_page/AboutSection.tsx
@@ -2,6 +2,77 @@ import ValueStatement from "../home_page/ValueStatement";
 
 const AboutSection = () => {
   return (
+
+<>
+
+
+<div className="    mx-auto prose max-w-none w-11/12 md:w-10/12   my-12  ">
+        <div className="  mx-auto md:grid md:grid-cols-3 md:gap-12 flex-col-reverse flex     ">
+        {/* <div className="  mx-auto sm:grid sm:grid-cols-1 lg:grid-cols-3 md:gap-12 order-last flex   "> */}
+
+        <div className="h-full w-full  object-cover  col-span-2 ">
+
+        <div className="overflow-hidden   w-full h-full">
+              <img
+                alt=""
+                className="h-full object-cover"
+                src="\images\projects\Ravensthorpe\cover_image.jpg"
+              />
+            </div>
+          {/* <ContactCarousel CarouselData={imagePaths}/> */}
+          </div>
+          {/* <img
+            alt=""
+            className="h-full object-cover "
+            src="\images\HomeHeroImages\08.jpg"
+          /> */}
+
+          <div className="my-auto mt-12">
+          <h1 className="text-left ">About if/Lab</h1>
+
+<p className=" lg:text-justify md:text-justify text-left  ">
+  if/LAB is a Perth-based design studio of transdisciplinary
+  architects, designers, technologists, software developers,
+  computational experts, and radical thinkers. We recognise the
+  urgency of adapting the way we work to enable accelerated
+  industry transformation that can deliver better, cheaper, and
+  faster design and technology enabled solutions for the built
+  environment.
+</p>
+<br className="hidden lg:visible" />
+<p>
+  We employ a creative, transdisciplinary, and data-informed
+  approach so that we can help to design a better future for all.
+  We believe in learning through exploring, creative solutions to
+  complex problems, partnering with experts across domains, and
+  using technology to provide end-to-end value through the
+  delivery of highly robust processes and better end-products.
+</p>
+<br className="hidden lg:visible" />
+<p>
+  We are experts in data, design, technology, research, strategy,
+  sustainability, and modern methods of construction utilising
+  prefabrication and advanced manufacturing.
+</p>
+<br className="hidden lg:visible" />
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* 
+
     <div className="prose max-w-none ">
       <div className="w-4/6 md:w-2/3 lg:mt-14 mt-8  mx-auto">
         <div>
@@ -9,6 +80,137 @@ const AboutSection = () => {
             <div className=" md:grid md:pt-12 lg:text-justify md:text-justify text-left  ">
               <h1 className="text-left ">About if/Lab</h1>
 
+              <p className=" lg:text-justify md:text-justify text-left  ">
+                if/LAB is a Perth-based design studio of transdisciplinary
+                architects, designers, technologists, software developers,
+                computational experts, and radical thinkers. We recognise the
+                urgency of adapting the way we work to enable accelerated
+                industry transformation that can deliver better, cheaper, and
+                faster design and technology enabled solutions for the built
+                environment.
+              </p>
+              <br className="hidden lg:visible" />
+              <p>
+                We employ a creative, transdisciplinary, and data-informed
+                approach so that we can help to design a better future for all.
+                We believe in learning through exploring, creative solutions to
+                complex problems, partnering with experts across domains, and
+                using technology to provide end-to-end value through the
+                delivery of highly robust processes and better end-products.
+              </p>
+              <br className="hidden lg:visible" />
+              <p>
+                We are experts in data, design, technology, research, strategy,
+                sustainability, and modern methods of construction utilising
+                prefabrication and advanced manufacturing.
+              </p>
+              <br className="hidden lg:visible" />
+
+
+
+</div>
+<div className="overflow-hidden   w-full h-full">
+              <img
+                alt=""
+                className="h-full object-cover"
+                src="\images\projects\Ravensthorpe\cover_image.jpg"
+              />
+            </div>
+          </div>
+                      </div>
+          
+        </div>
+      </div>
+ */}
+
+
+
+
+
+
+
+
+
+
+
+
+{/* 
+
+
+
+      <div className="m-auto">
+        <div className=" px-4 pt-14 m-auto h-full ">
+          <div className="mx-auto max-w-6xl flex flex-col md:flex-row   my-auto h-full">
+            
+          <h1 className="">
+                We ask <b className="underline">What if</b> we could change
+                future by changing building?
+              </h1>
+            <dl className="w-full h-full my-auto md:w-2/3">
+              <dt className="mb-2">
+                <h3 className="text-xl font-semibold"> We are <b>IMPACTFUL.</b></h3>
+              </dt>
+              <dd className="mb-8">
+                <p>
+                - Acknowledging the potential for positive change
+                  through design.
+                </p>
+              </dd>
+              <dt className="mb-2">
+                <h3 className="text-xl font-semibold">  We are <b>ETHICAL.</b></h3>
+              </dt>
+              <dd className="mb-8">
+                <p>
+                - Design for good and access for all.
+                </p>
+              </dd>
+              <dt className="mb-2">
+                <h3 className="text-xl font-semibold">  We are <b>DEFIANT.</b></h3>
+              </dt>
+              <dd className="mb-8">
+                <p>
+                - Challenging the status quo and always searching for a
+                  better process and outcome.
+                </p>
+              </dd>
+            </dl>
+          </div>
+      </div>
+    </div>
+ */}
+
+
+
+
+
+
+{/* 
+
+              <div className=" text-lg font-light">
+                <p>
+                  <span className="text-2xl font-semibold">
+                    {" "}
+                    We are <b>IMPACTFUL.</b>{" "}
+                  </span>{" "}
+                  <br />- Acknowledging the potential for positive change
+                  through design.
+                </p>
+                <p>
+                  <span className="text-2xl font-semibold">
+                    We are <b>ETHICAL.</b>{" "}
+                  </span>{" "}
+                  <br />- Design for good and access for all.
+                </p>
+                <p>
+                  <span className="text-2xl font-semibold">
+                    We are <b>DEFIANT.</b>{" "}
+                  </span>{" "}
+                  <br />- Challenging the status quo and always searching for a
+                  better process and outcome.
+                </p>
+              </div> */}
+              <br className="hidden lg:visible" />
+              {/* 
               <p className=" lg:text-justify md:text-justify text-left  ">
                 if/LAB is a Perth-based design studio of transdisciplinary
                 architects, designers, technologists, software developers,
@@ -32,19 +234,10 @@ const AboutSection = () => {
                 We are experts in data, design, technology, research, strategy,
                 sustainability, and modern methods of construction utilising
                 prefabrication and advanced manufacturing.
-              </p>
-            </div>
-            <div className="overflow-hidden   w-full h-full">
-              <img
-                alt=""
-                className="h-full object-cover"
-                src="\images\projects\Ravensthorpe\cover_image.jpg"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+              </p> */}
+
+   
+    </>
   );
 };
 export default AboutSection;
