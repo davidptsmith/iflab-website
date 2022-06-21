0973636dc23e2b5066a1e4b96710a6912b5890f4
David <davidptsmith@gmail.com>
2022-06-21 14:56:10 +0800

    added table item generation

--- components/Projects/ProjectTable.js
+++ components/Projects/ProjectTable.js
@@ -1,72 +1,31 @@
+
 const ProjectTable = ({ items }) => {
   return (
     <div>
-      {/*Table for data */}
       <dl className="grid grid-cols-1 gap-x-6 gap-y-10 md:grid-cols-1 lg:grid-cols-2 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
-        <div className="border-t border-gray-200 pt-4">
-          <dt className="font-medium text-gray-900">{items[0][0]}</dt>
-          <dd className="mt-2 text-sm -mb-4 md:-mb-8 text-gray-500">
-            {items[0][1]}
-          </dd>
-        </div>
-
-        <div className="border-t border-gray-200 pt-4">
-          <dt className="font-medium text-gray-900">{items[1][0]}</dt>
-          <dd className="mt-2 text-sm -mb-4 md:-mb-8 text-gray-500">
-            {items[1][1]}
-          </dd>
-        </div>
-
-        <div className="border-t border-gray-200 pt-4">
-          <dt className="font-medium text-gray-900">{items[2][0]}</dt>
-          <dd className="mt-2 text-sm -mb-4 md:-mb-8 text-gray-500 ">
-            {items[2][1]}
-          </dd>
-        </div>
-
-        <div className="border-t border-gray-200 pt-4">
-          <dt className="font-medium text-gray-900">{items[3][0]}</dt>
-          <dd className="mt-2 text-sm -mb-4 md:-mb-8 text-gray-500">
-            {items[3][1]}
-          </dd>
-        </div>
-
-        <div className="border-t border-gray-200 pt-4">
-          <dt className="font-medium text-gray-900">{items[4][0]}</dt>
-          <dd className="mt-2 text-sm -mb-4 md:-mb-8 text-gray-500">
-            {items[4][1]}
-          </dd>
-        </div>
-
-        {/* <div className="border-t border-gray-200 pt-4">
-          <dt className="font-medium text-gray-900">{items[5][0]}</dt>
-          <dd className="mt-2 text-sm -mb-4 md:-mb-8  text-gray-500">
-            {items[5][1]}
-          </dd>
-        </div> */}
-
-{/* add dynamic rendering to these elements if index exists */}
-{/* 
-        <div className="border-t border-gray-200 pt-4">
-          <dt className="font-medium text-gray-900">{items[6][0]}</dt>
-          <dd className="mt-2 text-sm -mb-4 md:-mb-8  text-gray-500">
-            {items[6][1]}
-          </dd>
-        </div>
-
-
-        <div className="border-t border-gray-200 pt-4">
-          <dt className="font-medium text-gray-900">{items[7][0]}</dt>
-          <dd className="mt-2 text-sm -mb-4 md:-mb-8  text-gray-500">
-            {items[7][1]}
-          </dd>
-        </div> */}
-
-
-
+        {showTableItems(items)}
       </dl>
     </div>
   );
 };
 
 export default ProjectTable;
+
+//Loop through items and get return html for table item
+function showTableItems(items) {
+  var html = [];
+  for (let i = 0; i < items.length; i++) {
+    html.push(returnTableHTML(items[i][0], items[i][1]));
+  }
+  return html;
+}
+
+// Generate html for table items 
+function returnTableHTML(heading, body) {
+  return (
+    <div className="border-t border-gray-200 pt-4">
+      <dt className="font-medium text-gray-900">{heading}</dt>
+      <dd className="mt-2 text-sm -mb-4 md:-mb-8 text-gray-500">{body}</dd>
+    </div>
+  );
+}
