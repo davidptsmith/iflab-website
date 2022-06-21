ac7d425610c0e40ce438f264fff0ce708161f515
David <davidptsmith@gmail.com>
2022-06-21 15:29:54 +0800

    added a loading message

---
+++ components/Utilities/CustomLoadingScreen.js
@@ -0,0 +1,10 @@
+export default function CustomLoadingScreen() {
+    return (
+        <div className="grid place-items-center z-50 h-screen w-screen ">
+          <h1 className="mx-auto my-auto text-[600px] underline text-gray-800">
+            if/lab 
+          </h1>
+        </div>
+    );
+  }
+  
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,40 @@
-import '../styles/globals.css'
-import 'tailwindcss/tailwind.css';
-import '../styles/tailwind.css'
-import type { AppProps } from 'next/app'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
-import Nav from '../components/NavBar/Nav'
+import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
+import "../styles/tailwind.css";
+import type { AppProps } from "next/app";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Nav from "../components/NavBar/Nav";
+
+import React from "react";
+import { useRouter } from "next/router";
+import CustomLoadingScreen from "../components/Utilities/CustomLoadingScreen";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [pageLoading, setPageLoading] = React.useState<boolean>(false);
+  React.useEffect(() => {
+    const handleStart = () => {
+      setPageLoading(true);
+    };
+    const handleComplete = () => {
+      setPageLoading(false);
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+  }, [router]);
+
   return (
-
     <>
-  
-  <Component {...pageProps} />
-
-  </>
-  )
+      {pageLoading ? (
+        <CustomLoadingScreen/>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
