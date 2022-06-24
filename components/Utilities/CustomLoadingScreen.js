65c0cf764cf7fa5f649c6f6f994fcf7d9ff8b871
David <davidptsmith@gmail.com>
2022-06-24 12:05:11 +0800

    updated loading text

--- components/Utilities/CustomLoadingScreen.js
+++ components/Utilities/CustomLoadingScreen.js
@@ -1,7 +1,11 @@
+import IflabLogo_SVG from "../IfLab_logo/IfLabLogoSVG-No-Hover";
+
 export default function CustomLoadingScreen() {
     return (
         <div className="grid place-items-center z-50 h-screen w-screen ">
-          <h1 className="animate-ping animate-pulse animate-bounce mx-auto my-auto text-[600px] underline font-light text-gray-300">
+          
+          {/* <IflabLogo_SVG className="w-96  fill-slate-700"/> */}
+          <h1 className=" animate-pulse mx-auto my-auto text-[200px] md:text-[600px] border-b-gray-100 border-2 font-extralight text-gray-100">
             if/lab 
           </h1>
         </div>
