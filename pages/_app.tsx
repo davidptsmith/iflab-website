04c30204aa641906ef1f3375ef4c21ed16d64fce
David <davidptsmith@gmail.com>
2022-06-29 13:58:36 +0800

    updated loading function

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,36 +5,38 @@ import type { AppProps } from "next/app";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Nav from "../components/NavBar/Nav";
+import Head from "next/head";
 
 import React from "react";
 import { useRouter } from "next/router";
+import {useState, useEffect} from 'react';
+import Router from 'next/router';
+
 import CustomLoadingScreen from "../components/Utilities/CustomLoadingScreen";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
-  const [pageLoading, setPageLoading] = React.useState<boolean>(false);
-  React.useEffect(() => {
-    const handleStart = () => {
-      setPageLoading(true);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    Router.events.on('routeChangeStart', () => setLoading(true));
+    Router.events.on('routeChangeComplete', () => setLoading(false));
+    Router.events.on('routeChangeError', () => setLoading(false));
+    return () => {
+      Router.events.off('routeChangeStart', () => setLoading(true));
+      Router.events.off('routeChangeComplete', () => setLoading(false));
+      Router.events.off('routeChangeError', () => setLoading(false));
     };
-    const handleComplete = () => {
-      setPageLoading(false);
-    };
-
-    router.events.on("routeChangeStart", handleStart);
-    router.events.on("routeChangeComplete", handleComplete);
-    router.events.on("routeChangeError", handleComplete);
-  }, [router]);
+  }, [Router.events]);
 
   return (
     <>
-    <head>
+    <Head>
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
       <meta httpEquiv="Content-Type" content="text/html; charset=ISO-8859-1"></meta>
       <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-    </head>
+    </Head>
 
-      {pageLoading ? (
+      {loading ? (
         <CustomLoadingScreen/>
       ) : (
         <Component {...pageProps} />
