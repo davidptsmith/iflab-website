071ad2060dd4e83955a2bd3f36b6297a0af04878
David <davidptsmith@gmail.com>
2022-07-18 17:39:12 +0800

    Centered and shortened text

--- components/home_page/ContactUs.tsx
+++ components/home_page/ContactUs.tsx
@@ -8,14 +8,16 @@ const ContactUs = () => {
     <div className="  min-h-[640px] xl:px-4  w-10/12 md:w-3/4  mx-auto z-10 grid grid-cols-1 gap-4  md:gap-12 xl:grid-cols-2">
       <div className="w-full xl:h-full h-full max-h-screen  overflow-hidden m-auto">
         <img
-          className="object-cover xl:h-full w-full  h-72 md:h-full max-h-screen "
-          src="/images/HomeHeroImages/05_ifLab_Architecture_Ravensthorpe.jpg"
-          alt={AltTextFromImage("05_ifLab_Architecture_Ravensthorpe")}
+          className="object-cover xl:h-full w-full  h-72 md:h-full max-h-[640] "
+          src="/images/HomePageImages/05_ifLab_Public_Art_Precinct.jpg"
+          alt={AltTextFromImage("05_ifLab_Public_Art_Precinct")}
         />
       </div>
 
-      <div className="xl:w-2/3 md:my-auto  xl:ml-16 xl:mt-12">
-        <div className="flex items-baseline prose ">
+      <div className="xl:w-2/3 md:my-auto  xl:ml-16 xl:mt-12 h-full flex">
+        <div className="md:m-auto">
+
+        <div className="flex items-baseline prose md:my-auto">
           {/* ////////////////////  update the link to contact page /////////////////// */}
 
           <h1 className="bottom-0 h-full  cursor-pointer text-left ">
@@ -32,7 +34,7 @@ const ContactUs = () => {
             We leverage innovation in design, technology, and manufacturing to
             enable equitable access to better design for all.
           </p>
-          <p className=" xl:text-justify md:text-justify text-left  ">
+          {/* <p className=" xl:text-justify md:text-justify text-left  ">
             By asking "what if?" we unlock new avenues for design and delivery
             across sectors and scales.
           </p>
@@ -40,22 +42,23 @@ const ContactUs = () => {
             Our team of architects, designers, computational experts, and
             software developers are ready to change the future by changing
             building, today.
-          </p>
+          </p> */}
           <br className="hidden lg:visible" />
         </div>
 
         <Link href="/Contact" passHref={true}>
           <div className="border-t-2 cursor-pointer">
             <h3>
-              We are always happy to assist <br />
+              {/* We are always happy to assist <br />
               others solver their problems.
-              <br />
-              Reach us here
+              <br /> */}
+              Contact us here
               <AiOutlineDoubleRight className="  p-1 text-2xl text-gray-600 inline-block cursor-pointer" />
             </h3>
           </div>
         </Link>
       </div>
+          </div>
     </div>
   );
 };
