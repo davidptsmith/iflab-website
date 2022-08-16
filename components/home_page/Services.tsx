701dfb2a6dbfbfb1e16a124bf46181bbf8e3eed7
David <davidptsmith@gmail.com>
2022-08-16 10:58:40 +0800

    added placeholder text

--- components/home_page/Services.tsx
+++ components/home_page/Services.tsx
@@ -26,12 +26,7 @@ const Services = () => {
               </dt>
               <dd className="mb-8">
                 <p>
-                  We don’t. Valohai isn’t a data science platform; it&#x27;s a
-                  Machine Learning Management Platform that handles the whole ML
-                  pipeline from feature extraction, to training of your model
-                  and to deploying it into production in a reproducible manner.
-                  Data science platforms offer hosted notebooks and AutoML
-                  solutions.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus rerum ratione culpa eum! A, eligendi exercitationem? Sapiente iure error explicabo id hic modi velit exercitationem ratione maiores, nulla in sit fugit laudantium laboriosam incidunt temporibus, maxime odio ipsa ex obcaecati voluptatibus eos mollitia esse. Eaque doloremque consectetur vitae quas accusamus?
                 </p>
               </dd>
               <dt className="mb-2">
@@ -39,12 +34,7 @@ const Services = () => {
               </dt>
               <dd className="mb-8">
                 <p>
-                  Depends. Most of our customers use their own cloud and thus
-                  pay for usage according to their own agreements. Valohai
-                  doesn&#x27;t charge anything on top of the per-user license
-                  fee. If you don&#x27;t have a cloud provider, you can use our
-                  AWS, GCP and Azure accounts, and we&#x27;ll only charge you
-                  for what you use.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus rerum ratione culpa eum! A, eligendi exercitationem? Sapiente iure error explicabo id hic modi velit exercitationem ratione maiores, nulla in sit fugit laudantium laboriosam incidunt temporibus, maxime odio ipsa ex obcaecati voluptatibus eos mollitia esse. Eaque doloremque consectetur vitae quas accusamus?
                 </p>
               </dd>
             </dl>
